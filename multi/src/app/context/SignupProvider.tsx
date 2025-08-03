515406243496645c5bf4b80df57c67005101996b
bhanukesh b <kesh.bhanu11@gmail.com>
2025-08-03 10:03:32 -0500

    updated codes

--- multi/src/app/context/SignupProvider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { SignupProvider } from './SignupContext';
-
-export default function SignupLayout({ children }: { children: React.ReactNode }) {
-  return <SignupProvider>{children}</SignupProvider>;
-}
